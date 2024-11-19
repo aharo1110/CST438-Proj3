da7c2f32583e4edcd8fdc39197397316c8dbb650
aharo1110 <aharo1110@gmail.com>
2024-11-18 21:48:42 -0800

    Users table updated and pets table created

--- backend/src/migrations/20241112191557_create_users.js
+++ backend/src/migrations/20241112191557_create_users.js
@@ -8,6 +8,9 @@ exports.up = function(knex) {
           return knex.schema.createTable('users', function (t) {
             t.increments('user_id').primary();
             t.string('username', 60);
+            t.string('display_name', 60);
+            t.integer('phone', 10);
+            t.string('email', 60);
             t.string('password', 60);
             t.boolean('is_admin').defaultTo(false);
           });
